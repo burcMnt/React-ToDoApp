2447ede9b3b89caf621fe3714e65261d83a53599
burcMnt <burcumantar0@gmail.com>
2021-10-14 12:40:31 +0300

    delete task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import React, { Component } from 'react';
 
 export class App extends Component {
@@ -21,18 +22,25 @@ export class App extends Component {
           <button>Add</button>
         </form>
         <ul className="todos-list">
-          {this.state.tasks.sort((a,b) => a.isDone==b.isDone ? a.task.localeCompare(b.task) : a.isDone-b.isDone)
-          .map((value, index) =>
-            <li key={index} className={value.isDone ? "done" : "undone"}>
-              <input type="checkbox" checked={value.isDone}
-                onChange={(e) => this.updateTodoStatus(e, index)} />
-              <span>{value.task} </span>
-
-            </li>
-          )}
+          {this.state.tasks.sort((a, b) => a.isDone == b.isDone ? a.task.localeCompare(b.task) : a.isDone - b.isDone)
+            .map((value, index) =>
+              <li key={index} className={"task-list-item " + (value.isDone ? 'done' : 'undone')} >
+                <input type="checkbox" checked={value.isDone}
+                  onChange={(e) => this.updateTodoStatus(e, index)} />
+                <span>{value.task} </span>
+                <a href="#" className="btn-delete-task" onClick={(e) => this.handleDelete(e, index)}><i className="fas fa-times"></i></a>
+              </li>
+            )}
         </ul>
-      </div>
+      </div >
     );
+  }
+
+  handleDelete(e,index) {
+    e.preventDefault();
+    const tasks=[...this.state.tasks];
+    tasks.splice(index, 1);
+    this.setState({tasks: tasks });
   }
 
   handleSubmit(e) {
